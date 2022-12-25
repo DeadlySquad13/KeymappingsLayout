921055812526f8217a697b89afc5f589617a0ea4
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-25 19:09:11 +0300

    [Enh](Keymappings): Added store.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import cnBind from 'classnames/bind';
 
 import { KeyboardLayout } from 'components/KeyboardLayout';
+import { keymappingsStore } from 'components/KeyboardLayout/KeyboardLayout.store';
 
 import styles from './App.module.scss';
+import { observable } from 'mobx';
 
 const cx = cnBind.bind(styles);
 
-export const App: React.FC = () => {
+export const App = observable(() => {
     return (
         <div className={cx('App')}>
             <header>
             </header>
-            <body className={cx('App__Body')}>
+            <aside>
+                <header>{keymappingsStore.keys}</header>
+                <div>
+                    Info about keymappings.
+                </div>
+                <footer />
+            </aside>
+            <body>
                 <KeyboardLayout title="NeoVim" />
                 <KeyboardLayout title="WezTerm" />
             </body>
         </div>
     );
-};
+});
--- src/components/KeyboardLayout/KeyboardLayout.tsx
+++ src/components/KeyboardLayout/KeyboardLayout.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import Keyboard, { SimpleKeyboard } from "react-simple-keyboard";
 import cnBind from 'classnames/bind';
+import { KeymappingsContext } from "contexts/KeymappingsContext";
+import { observer } from 'mobx-react-lite';
 import { SimpleKeyboardLayouts } from 'simple-keyboard-layouts';
+
+import { DISPLAY, LAYOUT_TYPE } from './KeyboardLayout.constants'
+import { keymappingsStore } from "./KeyboardLayout.store";
+import { LayoutType } from "./KeyboardLayout.types";
 
 import "react-simple-keyboard/build/css/index.css";
 import styles from "./KeyboardLayout.module.scss";
@@ -12,11 +18,6 @@ export type KeyboardLayoutProps = React.HTMLAttributes<HTMLDivElement> & {
     title?: string
 }
 
-enum LayoutType {
-    Big,
-    Small,
-    MacOs,
-}
 const getLayout = (layoutType: LayoutType = LayoutType.Big) => {
     const { layout } = keyboard.layouts.english;
 
@@ -24,17 +25,18 @@ const getLayout = (layoutType: LayoutType = LayoutType.Big) => {
         case LayoutType.Big: {
             return {
                 default: [
-                    "esc [F1 F2 F3 F4] [F5 F6 F7 F8] F9 F10 F11 F12",
-                    "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+                    "{esc} [F1 F2 F3 F4] [F5 F6 F7 F8] F9 F10 F11 F12",
+                    "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
                     "{tab} q w e r t y u i o p [ ] {enter}",
-                    "{lock} a s d f g h j k l ; ' \\ {enter}",
+                    "{capslock} a s d f g h j k l ; ' \\ {enter}",
                     "{shift} \\ z x c v b n m , . / {shift}",
                     "{controlleft} {winleft} {altleft} {space} {altright} {winright} {app} {controlright}",
                 ],
                 shift: [
-                    "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+                    "{esc} [F1 F2 F3 F4] [F5 F6 F7 F8] F9 F10 F11 F12",
+                    "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
                     "{tab} Q W E R T Y U I O P { } {enter}",
-                    "{lock} A S D F G H J K L : \" | {enter}",
+                    "{capslock} A S D F G H J K L : \" | {enter}",
                     "{shift} | Z X C V B N M < > ? {shift}",
                     "{controlleft} {winleft} {altleft} {space} {altright} {winright} {app} {controlright}",
                 ]
@@ -56,40 +58,19 @@ const getLayout = (layoutType: LayoutType = LayoutType.Big) => {
 
 const keyboard = new SimpleKeyboardLayouts();
 
-const display = {
-    '{escape}': 'esc ⎋',
-    '{tab}': 'tab ⇥',
-    '{backspace}': 'backspace ⌫',
-    '{bksp}': 'backspace ⌫',
-    '{enter}': 'enter ↵',
-    '{capslock}': 'caps lock ⇪',
-    '{lock}': 'caps lock ⇪',
-    '{shift}': 'shift ⇧',
-    '{shiftleft}': 'shift ⇧',
-    '{shiftright}': 'shift ⇧',
-    '{controlleft}': 'ctrl ⌃',
-    '{controlright}': 'ctrl ⌃',
-    '{altleft}': 'alt ⌥',
-    '{altright}': 'alt ⌥',
-    '{metaleft}': 'cmd ⌘',
-    '{winleft}': 'win ⊞',
-    '{winright}': 'win ⊞',
-    '{metaright}': 'cmd ⌘',
-    '{space}': ' ',
-    '{app}': 'app ≣',
-}
+const layout = getLayout(LAYOUT_TYPE)
 
-const layoutType = LayoutType.Big;
-const layout = getLayout(layoutType)
-
-export const KeyboardLayout: React.VFC<KeyboardLayoutProps> = (props) => {
+export const KeyboardLayout = observer((props: KeyboardLayoutProps) => {
     const [input, setInput] = useState("");
-    const [layoutName, setLayoutName] = useState("default");
+    const [layoutName, setLayoutName] = useState('default');
     const keyboard = useRef<SimpleKeyboard>();
 
+    const { setKeymapping } = useContext(KeymappingsContext)
+
     const onChange = (input: string) => {
+        keymappingsStore.keys = input.split('')
+        console.log(keymappingsStore.children);
         setInput(input);
-        console.log("Input changed", input);
     };
 
     const handleShift = () => {
@@ -97,24 +78,25 @@ export const KeyboardLayout: React.VFC<KeyboardLayoutProps> = (props) => {
         setLayoutName(newLayoutName);
     };
 
-    const onKeyPress = (button: unknown) => {
-        console.log("Button pressed", button);
+    const onKeyPress = (button: string) => {
+        setKeymapping?.(() => button);
 
-        /**
-         * If you want to handle the shift and caps lock buttons
-         */
-        if (button === "{shift}" || button === "{lock}") handleShift();
+        console.log({ button })
+        if (button === '{backspace}') {
+            keymappingsStore.pop()
+        } else {
+            keymappingsStore.push(button)
+        }
+
+        // Handle the shift and caps lock buttons.
+        if (button === '{shift}' || button === '{lock}') handleShift();
     };
 
     const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         const input = event.target.value;
-        setInput(input);
+        onChange(input);
         keyboard.current?.setInput(input);
     };
-
-    useEffect(() => {
-        console.log({ layout })
-    }, [])
 
     return (
         <div className={cx('KeyboardLayout', props.className)}>
@@ -130,7 +112,7 @@ export const KeyboardLayout: React.VFC<KeyboardLayoutProps> = (props) => {
                 theme={"hg-theme-default hg-layout-default myTheme"}
                 layoutName={layoutName}
                 layout={layout}
-                display={display}
+                display={DISPLAY}
                 onChange={onChange}
                 onKeyPress={onKeyPress}
                 buttonTheme={[
@@ -141,9 +123,13 @@ export const KeyboardLayout: React.VFC<KeyboardLayoutProps> = (props) => {
                     {
                         class: cx("hg-highlight"),
                         buttons: "Q q"
-                    }
+                    },
+                    {
+                        class: cx('hg-chord'),
+                        buttons: keymappingsStore.childrenFormatted
+                    },
                 ]}
             />
         </div>
     );
-}
+});
